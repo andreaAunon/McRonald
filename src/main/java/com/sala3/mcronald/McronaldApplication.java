9b54cc00fa8f29ab421626fad7060fc6fcdd92de
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-14 10:19:39 +0100

    initial commit 2

---
+++ src/main/java/com/sala3/mcronald/McronaldApplication.java
@@ -0,0 +1,13 @@
+package com.sala3.mcronald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McronaldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McronaldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sala3/mcronald/McronaldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sala3.mcronald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McronaldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
