aea3840d81ce3b630f167635d5ccd7472a07aa93
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-14 11:02:25 +0100

    menu created

---
+++ src/main/java/com/sala3/mcronald/entities/Menu.java
@@ -0,0 +1,13 @@
+package com.sala3.mcronald.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "menu")
+@Table(name = "MENU")
+public class Menu {
+}
